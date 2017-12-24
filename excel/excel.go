645c82ebf716a5c9d26c958b942f74d944f7f536
Sudhanshu Raheja <sudhanshu@go-jek.com>
2017-12-25 01:56:10 +0700

    Tried with excel, the first page was loading fine, but only partial data in other pages

--- excel/excel.go
+++ excel/excel.go
@@ -6,8 +6,39 @@ import (
 	"github.com/extrame/xls"
 )
 
+// Load : Try reading from the excel
 func Load() {
-	if xlFile, err := xls.Open("Table.xls", "utf-8"); err == nil {
-		fmt.Println(xlFile.Author)
+	fmt.Println("Running this now")
+
+	fileName := "../data/sample.xls"
+	encoding := "utf-8"
+
+	workBook, err := xls.Open(fileName, encoding)
+	if err != nil {
+		fmt.Println("There was an error in opening the file:", err)
+		return
+	}
+
+	sheetNo := -1
+	for {
+		sheetNo++
+		sheet := workBook.GetSheet(sheetNo)
+		if sheet == nil {
+			break
+		}
+
+		fmt.Println("Found a new sheet [", sheet.Name, "], Total lines: ", sheet.MaxRow)
+		// 2 should be int(sheet.MaxRow)
+		for i := 1; i < 10; i++ {
+			row := sheet.Row(i)
+			if row == nil {
+				continue
+			}
+
+			for col := row.FirstCol(); col < row.LastCol(); col++ {
+				fmt.Printf("%s, ", row.Col(col))
+			}
+			fmt.Println("\n")
+		}
 	}
 }
