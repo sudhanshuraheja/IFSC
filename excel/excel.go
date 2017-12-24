0d8dec91b17960420ecbbe69b04f176238106e10
Sudhanshu Raheja <sudhanshu@go-jek.com>
2017-12-25 02:05:21 +0700

    Moved completely to the xlsx package

--- excel/excel.go
+++ excel/excel.go
@@ -3,42 +3,27 @@ package excel
 import (
 	"fmt"
 
-	"github.com/extrame/xls"
+	"github.com/tealeg/xlsx"
 )
 
 // Load : Try reading from the excel
 func Load() {
 	fmt.Println("Running this now")
 
-	fileName := "../data/sample.xls"
-	encoding := "utf-8"
+	fileName := "../data/sample.xlsx"
 
-	workBook, err := xls.Open(fileName, encoding)
+	workBook, err := xlsx.OpenFile(fileName)
 	if err != nil {
 		fmt.Println("There was an error in opening the file:", err)
 		return
 	}
 
-	sheetNo := -1
-	for {
-		sheetNo++
-		sheet := workBook.GetSheet(sheetNo)
-		if sheet == nil {
-			break
-		}
-
-		fmt.Println("Found a new sheet [", sheet.Name, "], Total lines: ", sheet.MaxRow)
-		// 2 should be int(sheet.MaxRow)
-		for i := 1; i < 10; i++ {
-			row := sheet.Row(i)
-			if row == nil {
-				continue
+	for _, sheet := range workBook.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				text := cell.String()
+				fmt.Printf("%s\n", text)
 			}
-
-			for col := row.FirstCol(); col < row.LastCol(); col++ {
-				fmt.Printf("%s, ", row.Col(col))
-			}
-			fmt.Println("\n")
 		}
 	}
 }
