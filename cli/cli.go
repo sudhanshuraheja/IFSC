3bb94b4a5d809e3702133f7e5ea7a6ac2f509e2f
Sudhanshu Raheja <sudhanshu@go-jek.com>
2017-12-25 12:06:06 +0700

    Migrated cli to a separate package

---
+++ cli/cli.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sudhanshuraheja/ifsc/config"
+	"github.com/urfave/cli"
+)
+
+// Start : start the cli wrapper
+func Start() {
+	app := cli.NewApp()
+	app.Name = config.Name()
+	app.Version = config.Version()
+	app.Usage = "this service lists all bank branches and ifsc codes in india"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "start",
+			Usage: "start the service",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Start the service")
+				return nil
+			},
+		},
+		{
+			Name:  "stop",
+			Usage: "stop the service",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Stop the service")
+				return nil
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+}
