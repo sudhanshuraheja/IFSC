df071b859e8b69eced6ebcb9e0789e2f9a157c8b
Sudhanshu Raheja <sudhanshu@go-jek.com>
2017-12-25 15:00:40 +0700

    Added middleware for gzip and delay, setup server with negroni

--- server/server.go
+++ server/server.go
@@ -1,15 +1,27 @@
 package server
 
 import (
+	"net/http"
+
+	"github.com/jeffbmartinez/delay"
+	"github.com/phyber/negroni-gzip/gzip"
 	"github.com/urfave/negroni"
 )
 
 // StartAPIServer : setup routes and start the server
 func StartAPIServer() {
-	server := negroni.New(negroni.NewRecovery())
+	server := negroni.New()
 	router := Router()
 
+	server.Use(negroni.NewRecovery())
+	server.Use(negroni.NewLogger())
+	server.Use(delay.Middleware{})
+	server.Use(gzip.Gzip(gzip.DefaultCompression))
+	// TODO : Move this behind remote config
+	// Usually not required
+	// server.Use(negroni.NewStatic(http.Dir("public")))
 	server.Use(Recover())
 	server.UseHandler(router)
-	server.Run(":3000")
+
+	http.ListenAndServe(":3000", server)
 }
