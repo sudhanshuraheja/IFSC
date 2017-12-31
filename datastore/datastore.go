602212473e68a11beec48e48792d2cdff6d01dd8
Sudhanshu Raheja <sudhanshu@go-jek.com>
2017-12-31 14:46:35 +0700

    Removed unwanted code from datastore

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,41 +1,27 @@
 package datastore
 
 import (
-	"strings"
-
 	"github.com/sudhanshuraheja/ifsc/excel"
-	"github.com/sudhanshuraheja/ifsc/logger"
 )
 
-type store struct {
-	branches excel.Branches
-}
-
-var branchStore store
-
-// Init : initialise the datastore
-func Init(dataFile string) {
-	branchStore.branches = excel.Load(dataFile)
-	logger.Infoln("Loaded up data for", branchStore.branches.Count, "banks")
+// Branch : struct for the data in branch table
+type Branch struct {
+	DBId      int64  `db:"id"`
+	Bank      string `db:"bank"`
+	Ifsc      string `db:"ifsc"`
+	Micr      string `db:"micr"`
+	Branch    string `db:"branch"`
+	Address   string `db:"address"`
+	City      string `db:"city"`
+	District  string `db:"district"`
+	State     string `db:"state"`
+	Contact   string `db:"contact"`
+	CreatedAt string `db:"created_at"`
+	UpdatedAt string `db:"updated_at"`
 }
 
 // Search : search the datastore
 func Search(query string) []excel.Branch {
 	results := []excel.Branch{}
-	maxResults := 5
-
-	if query == "" {
-		return results
-	}
-
-	for _, bank := range branchStore.branches.List {
-		if len(results) >= maxResults {
-			break
-		}
-		if strings.Contains(strings.ToLower(bank.FullText), strings.ToLower(query)) {
-			results = append(results, bank)
-		}
-	}
-
 	return results
 }
