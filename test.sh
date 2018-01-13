03ea6b7dabd04867963ad1c2b9004ff3df0442ec
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 00:02:00 +0700

    Renamed coverate.out to coverage.txt, apparently that's what codecov expects

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.out
+echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     echo "go test -covermode=atomic -coverprofile=profile.out  $d\n"
     go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
-        cat profile.out >> coverage.out
+        cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
 
-go tool cover -html=coverage.out -o coverage.html
+go tool cover -html=coverage.txt -o coverage.html
