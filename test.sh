654189c4356e7de6525639ff8f34fdd6338cba5a
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 00:22:23 +0700

    Moved to using count as covermode

--- test.sh
+++ test.sh
@@ -4,12 +4,10 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    echo "go test -covermode=atomic -coverprofile=profile.out  $d\n"
+    echo "go test -covermode=count -coverprofile=profile.out $d\n"
     go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
-
-go tool cover -html=coverage.txt -o coverage.html
