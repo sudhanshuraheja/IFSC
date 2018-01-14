e6d8055a8240332e44496d88d59e7baa269794a5
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 12:50:42 +0700

    Naming changes to db.go

--- db/db.go
+++ db/db.go
@@ -15,36 +15,36 @@ const (
 	defaultTimeout  = 1 * time.Second
 )
 
-var db *sqlx.DB
+var database *sqlx.DB
 
 // Init : Initialiase the database connection
 func Init() {
 	var err error
 
-	db, err = sqlx.Open("postgres", config.Database().ConnectionString())
+	database, err = sqlx.Open("postgres", config.Database().ConnectionString())
 	if err != nil {
 		log.Fatalf("Could not connect to database: %s", err)
 	} else {
 		logger.Debug("Connected to database")
 	}
 
-	if err = db.Ping(); err != nil {
+	if err = database.Ping(); err != nil {
 		log.Fatalf("Ping to the database failed: %s on connString %s", err, config.Database().ConnectionString())
 	}
 
-	db.SetMaxIdleConns(config.Database().MaxPoolSize())
-	db.SetMaxOpenConns(config.Database().MaxPoolSize())
-	db.SetConnMaxLifetime(connMaxLifetime)
+	database.SetMaxIdleConns(config.Database().MaxPoolSize())
+	database.SetMaxOpenConns(config.Database().MaxPoolSize())
+	database.SetConnMaxLifetime(connMaxLifetime)
 	logger.Infoln("DB.Init() has been successfully done")
 }
 
 // Close : close the db connection
 func Close() error {
 	logger.Info("Closing the DB connection")
-	return db.Close()
+	return database.Close()
 }
 
 // Get : get a reference to the database connection
 func Get() *sqlx.DB {
-	return db
+	return database
 }
