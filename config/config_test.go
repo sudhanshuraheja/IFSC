2825e05d90f46b86b0c2823dfbcddb8d515f0805
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 12:56:27 +0700

    Using config_test package for config_test

--- config/config_test.go
+++ config/config_test.go
@@ -1,14 +1,16 @@
-package config
+package config_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/sudhanshuraheja/ifsc/config"
 )
 
 func Test_ConfigValues(t *testing.T) {
-	Load()
-	assert.Equal(t, "ifsc", Name())
-	assert.Equal(t, "dbname=ifsc user=ifsc password='ifsc_postgres' host=db port=5432 sslmode=disable", config.database.ConnectionString())
-	assert.Equal(t, "postgres://ifsc:ifsc_postgres@db:5432/ifsc?sslmode=disable", config.database.ConnectionURL())
+	config.Load()
+	assert.Equal(t, "ifsc", config.Name())
+
+	assert.Equal(t, "dbname=ifsc user=ifsc password='ifsc_postgres' host=db port=5432 sslmode=disable", config.Database().ConnectionString())
+	assert.Equal(t, "postgres://ifsc:ifsc_postgres@db:5432/ifsc?sslmode=disable", config.Database().ConnectionURL())
 }
