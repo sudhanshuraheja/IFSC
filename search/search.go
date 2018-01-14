c74920907f471abd8df2bcf02bf5bb62d7913ccf
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 15:08:26 +0700

    Updating functions which are now in utils

--- search/search.go
+++ search/search.go
@@ -1,9 +1,7 @@
 package search
 
 import (
-	"strings"
-
-	porterstemmer "github.com/reiver/go-porterstemmer"
+	"github.com/sudhanshuraheja/ifsc/utils"
 )
 
 // Item : each item that needs to be indexed
@@ -24,70 +22,13 @@ type SubItem struct {
 func (i *Item) AddIndex() {
 	i.Index = make(map[string]int)
 	for _, x := range i.SubItems {
-		i.Index = mergeMaps(i.Index, addSubItemIndex(x.Value, x.Weight))
+		i.Index = utils.MergeMaps(i.Index, addSubItemIndex(x.Value, x.Weight))
 	}
 }
 
 // AddSubItem : Add a subItem from an external file
 func addSubItemIndex(value string, weight int) map[string]int {
-	words := splitWords(value)
-	frequency := wordFrequencyCounter(stemWords(words), weight)
+	words := utils.SplitWords(value)
+	frequency := wordFrequencyCounter(utils.StemWords(words), weight)
 	return frequency
-}
-
-func splitWords(line string) []string {
-	line = strings.ToLower(line)
-	splitList := []string{" ", ",", ":", "-", "(", ")", ".", "'"}
-	words := strings.FieldsFunc(line, func(r rune) bool {
-		success := false
-		for _, split := range splitList {
-			if string(r) == split {
-				success = true
-			}
-		}
-		return success
-	})
-	return words
-}
-
-func stemWords(words []string) []string {
-	output := []string{}
-	for _, word := range words {
-		output = append(output, porterstemmer.StemString(word))
-	}
-	return output
-}
-
-func wordFrequencyCounter(words []string, weight int) map[string]int {
-	frequencyCounter := make(map[string]int)
-
-	for _, word := range words {
-		_, wordExists := frequencyCounter[word]
-		if wordExists {
-			frequencyCounter[word] = frequencyCounter[word] + (1 * weight)
-		} else {
-			frequencyCounter[word] = (1 * weight)
-		}
-	}
-
-	return frequencyCounter
-}
-
-func mergeMaps(map1 map[string]int, map2 map[string]int) map[string]int {
-	result := make(map[string]int)
-
-	for key, value := range map1 {
-		result[key] = value
-	}
-
-	for key := range map2 {
-		_, keyExists := map2[key]
-		if keyExists {
-			result[key] = result[key] + map2[key]
-		} else {
-			result[key] = map2[key]
-		}
-	}
-
-	return result
 }
--- search/search_test.go
+++ search/search_test.go
@@ -8,40 +8,6 @@ import (
 	"github.com/sudhanshuraheja/ifsc/logger"
 )
 
-func Test_splitWords(t *testing.T) {
-	words := splitWords("hello world")
-	assert.Equal(t, len(words), 2)
-	assert.Equal(t, words[0], "hello")
-	assert.Equal(t, words[1], "world")
-
-	words = splitWords("This  is  the  end, Beautiful friend, This is the-end, My only friend, The end, Of our e:laborate plans, the end, Of everything that stands, the end, No safety or surprise, the end. I'll never look into your eyes (again)")
-	expectedWords := []string{"this", "is", "the", "end", "beautiful", "friend", "this", "is", "the", "end", "my", "only", "friend", "the", "end", "of", "our", "e", "laborate", "plans", "the", "end", "of", "everything", "that", "stands", "the", "end", "no", "safety", "or", "surprise", "the", "end", "i", "ll", "never", "look", "into", "your", "eyes", "again"}
-	assert.Equal(t, words, expectedWords)
-}
-
-func Test_stemWords(t *testing.T) {
-	words := []string{"the", "runner", "is", "running", "runs"}
-	output := stemWords(words)
-	assert.Equal(t, output, []string{"the", "runner", "is", "run", "run"})
-}
-
-func Test_wordFrequencyCounter(t *testing.T) {
-	words := []string{"this", "is", "the", "end", "beautiful", "friend", "this", "is", "the", "end", "my", "only", "friend", "the", "end", "of", "our", "e", "laborate", "plans", "the", "end", "of", "everything", "that", "stands", "the", "end", "no", "safety", "or", "surprise", "the", "end", "i", "ll", "never", "look", "into", "your", "eyes", "again"}
-	expectedFrequency := map[string]int{"again": 1, "beautiful": 1, "e": 1, "end": 6, "everything": 1, "eyes": 1, "friend": 2, "i": 1, "into": 1, "is": 2, "laborate": 1, "look": 1, "ll": 1, "my": 1, "never": 1, "no": 1, "of": 2, "only": 1, "or": 1, "our": 1, "plans": 1, "safety": 1, "surprise": 1, "stands": 1, "the": 6, "that": 1, "this": 2, "your": 1}
-	frequency := wordFrequencyCounter(words, 1)
-	assert.Equal(t, frequency, expectedFrequency)
-
-	words = []string{"one", "one", "two"}
-	frequency = wordFrequencyCounter(words, 2)
-	assert.Equal(t, frequency, map[string]int{"one": 4, "two": 2})
-}
-
-func Test_mergeMaps(t *testing.T) {
-	map1 := map[string]int{"one": 1, "two": 2, "three": 3}
-	map2 := map[string]int{"two": 2, "three": 3, "four": 4}
-	assert.Equal(t, mergeMaps(map1, map2), map[string]int{"one": 1, "two": 4, "three": 6, "four": 4})
-}
-
 func Test_buildIndex(t *testing.T) {
 
 	inputSubitems := []SubItem{
