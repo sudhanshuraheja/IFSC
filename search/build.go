e2f3d007babde9a53966040100097049a5c64572
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-14 21:11:04 +0800

    Separated search into build and find

---
+++ search/build.go
@@ -0,0 +1,34 @@
+package search
+
+import (
+	"github.com/sudhanshuraheja/ifsc/utils"
+)
+
+// Item : each item that needs to be indexed
+type Item struct {
+	ID       int64
+	SubItems []SubItem
+	Index    map[string]int
+}
+
+// SubItem : each component inside an item that can have a separate weight
+type SubItem struct {
+	Key    string
+	Value  string
+	Weight int
+}
+
+// AddIndex : add index for a specific item
+func (i *Item) AddIndex() {
+	i.Index = make(map[string]int)
+	for _, x := range i.SubItems {
+		i.Index = utils.MergeMaps(i.Index, addSubItemIndex(x.Value, x.Weight))
+	}
+}
+
+// AddSubItem : Add a subItem from an external file
+func addSubItemIndex(value string, weight int) map[string]int {
+	words := utils.SplitWords(value)
+	frequency := utils.WordFrequencyCounter(utils.StemWords(words), weight)
+	return frequency
+}
--- search/search.go
+++ search/search.go
@@ -1,34 +1,76 @@
 package search
 
 import (
-	"github.com/sudhanshuraheja/ifsc/utils"
+	"errors"
+	"fmt"
+
+	"github.com/sudhanshuraheja/ifsc/db"
+	"github.com/sudhanshuraheja/ifsc/logger"
+	"github.com/sudhanshuraheja/ifsc/model"
+
+	porterstemmer "github.com/reiver/go-porterstemmer"
 )
 
-// Item : each item that needs to be indexed
-type Item struct {
-	ID       int64
-	SubItems []SubItem
-	Index    map[string]int
+// WordIndex : struct for indexes in the database
+type WordIndex struct {
+	ID       int64  `db:"id"`
+	Key      string `db:"key"`
+	BranchID int64  `db:"branch"`
+	Weight   int    `db:"weight"`
 }
 
-// SubItem : each component inside an item that can have a separate weight
-type SubItem struct {
-	Key    string
-	Value  string
-	Weight int
+func (w *WordIndex) toString() string {
+	return fmt.Sprintln("[", w.ID, "]", w.Key, w.BranchID, w.Weight)
 }
 
-// AddIndex : add index for a specific item
-func (i *Item) AddIndex() {
-	i.Index = make(map[string]int)
-	for _, x := range i.SubItems {
-		i.Index = utils.MergeMaps(i.Index, addSubItemIndex(x.Value, x.Weight))
+// AddLookup : add indexes for a particular item to the index
+func (w *WordIndex) AddLookup(i Item) error {
+	branchid := i.ID
+	database := db.Get()
+
+	for key, weight := range i.Index {
+
+		_, err := database.Exec("INSERT INTO search (key, branch, weight) VALUES ($1, $2, $3) ON CONFLICT (key, branch) DO UPDATE SET weight = $3 WHERE search.key = $1 and search.branch = $2", key, branchid, weight)
+		if err != nil {
+			logger.Fatalln("Pushing to DB for", key, branchid, weight, "failed with error", err)
+			return err
+		}
 	}
+	return nil
 }
 
-// AddSubItem : Add a subItem from an external file
-func addSubItemIndex(value string, weight int) map[string]int {
-	words := utils.SplitWords(value)
-	frequency := utils.WordFrequencyCounter(utils.StemWords(words), weight)
-	return frequency
+func (w *WordIndex) findKey(query string) (string, error) {
+	stemmedKey := porterstemmer.StemString(query)
+	return stemmedKey, nil
+}
+
+// Find : find list of items which match this query
+func (w *WordIndex) Find(query string) ([]model.Branch, error) {
+	results := []model.Branch{}
+	key, err := w.findKey(query)
+	if err != nil {
+		return []model.Branch{}, errors.New("We could not find any search results for " + query)
+	}
+
+	database := db.Get()
+	rows, err := database.Queryx("SELECT * FROM branches WHERE id IN (SELECT branch FROM SEARCH WHERE key=$1 ORDER BY weight DESC LIMIT 25)", key)
+
+	if err != nil {
+		logger.Debugln("Error in query", err)
+		return results, nil
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var b model.Branch
+		err = rows.StructScan(&b)
+
+		if err != nil {
+			logger.Debugln("Error is parsing row", err)
+		}
+
+		results = append(results, b)
+	}
+
+	return results, nil
 }
